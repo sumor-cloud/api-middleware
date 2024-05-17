7c78dcd2e35ba0cb4a6b6047f06754f60d0c2755
dennis <dennis@sumor.com>
2024-05-17 09:49:49 +0800

    prettier

--- src/load.js
+++ src/load.js
@@ -1,6 +1,6 @@
 import { findReference } from '@sumor/config'
 
-export default async (root) => {
+export default async root => {
   const meta = await findReference(`${root}`, ['js'])
   console.log(`${root}`, meta)
   return meta
