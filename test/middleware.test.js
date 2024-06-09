7da5c65bc6f5be9c7d39047242138fa6b956e946
Dennis Chen <70829793@qq.com>
2024-06-09 22:02:56 +0800

    refactor

--- test/middleware.test.js
+++ test/middleware.test.js
@@ -2,7 +2,6 @@
 
 import { describe, expect, it } from '@jest/globals'
 import bodyParser from '../src/middleware/bodyParser.js'
-import fileParser from '../src/middleware/fileParser.js'
 import exposeApi from '../src/middleware/exposeApi.js'
 import createApp from '@sumor/ssl-server'
 import axios from 'axios'
@@ -15,8 +14,8 @@ describe('middleware', () => {
     const app = createApp()
 
     try {
-      app.use(bodyParser)
-      app.post('/data', (req, res) => {
+      app.all('/data', bodyParser())
+      app.all('/data', (req, res) => {
         res.send(req.data)
       })
       await app.listen(port)
@@ -42,10 +41,11 @@ describe('middleware', () => {
     const app = createApp()
 
     try {
-      app.use(fileParser([{ name: 'file' }]))
-      app.post('/data', async (req, res) => {
+      app.all('/data', bodyParser([{ name: 'file' }]))
+      app.all('/data', async (req, res) => {
         const fileInfo = req.data.file[0]
         fileInfo.content = await fse.readFile(fileInfo.path, 'utf8')
+        fileInfo.params = { a: req.data.a, b: req.data.b }
         res.send(fileInfo)
       })
       await app.listen(port)
@@ -54,10 +54,11 @@ describe('middleware', () => {
       const formData = new FormData()
       const file = new Blob(['OK'], { type: 'text/plain' })
       formData.append('file', file, 'file.txt')
+      formData.append('b', 2)
 
       const response = await axios({
         method: 'post',
-        url: `https://localhost:${port}/data`,
+        url: `https://localhost:${port}/data?a=1`,
         data: formData,
         headers: {
           'Content-Type': 'multipart/form-data'
@@ -67,6 +68,7 @@ describe('middleware', () => {
 
       expect(response.data.name).toEqual('file.txt')
       expect(response.data.content).toEqual('OK')
+      expect(response.data.params).toEqual({ a: '1', b: '2' })
 
       await app.close()
     } catch (e) {
@@ -81,12 +83,15 @@ describe('middleware', () => {
       app.use(
         exposeApi({
           'api.add': {
+            route: '/api/add',
             name: 'add'
           },
           'api.plus': {
+            route: '/api/plus',
             name: 'plus'
           },
           'api.minus': {
+            route: '/api/minus',
             name: 'minus'
           }
         })
